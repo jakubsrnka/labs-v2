1aa840e9b13b780d1e701a9172f2d99e1a43c0df
Jakub Srnka <jakub.srnka@volny.cz>
2025-06-23 12:50:51 +0200

    ✨ testing iframe for B21

---
+++ src/routes/b21/embed/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex h-dvh w-dvw items-center justify-center">
+	<iframe
+		src="https://dev.bible21.cz/embedded/reader/genesis"
+		title="Bible21 Čtečka"
+		class="h-full max-h-full w-full max-w-full border md:h-4/5 md:w-4/5"
+	></iframe>
+</div>
